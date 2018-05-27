8b9737bd1c46877c86b42dfa4ce9fe56f384d31b
David Muro <david.muro9@gmail.com>
2018-05-27 18:59:56 +0200

    added gps location

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Geolocation from './components/Geolocation';
+
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Geolocation />
+  </View>
+)
+
+export default App;
